a35036ef5d372e01cf00899d47d141e33395793f
huangshaozhe <97889667+huangshaozhe@users.noreply.github.com>
2022-07-01 20:39:08 +0800

    修改模块安装器

--- module_installer.py
+++ module_installer.py
@@ -1,4 +1,5 @@
 #*-*coding:utf-8*-*
+import os
 os.system("pip install eyed3")
 os.system("pip install jsonpath")
 os.system("pip install mutagen")
@@ -7,3 +8,4 @@ os.system("pip install pyqt5-tools")
 os.system("pip install qtawesome")
 os.system("pip install pygame")
 os.system("pip install pyinstaller")
+os.system("pip config set pip config set global.index-url http://mirrors.aliyun.com/pypi/simple/")
